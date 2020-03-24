da392c9dea1930f2aae76ccb9738fe34bab32b69
vitek999 <vitek2012rus@gmail.com>
2020-03-24 16:05:26 +0400

    Implement battery release charge

--- src/ru/poas/robots/Battery.java
+++ src/ru/poas/robots/Battery.java
@@ -1,10 +1,17 @@
 package ru.poas.robots;
 
 public class Battery {
+
     private Integer charge;
 
-    public Integer releaseCharge() {
-        throw new UnsupportedOperationException();
+    public Battery(Integer charge) {
+        this.charge = charge;
+    }
+
+    public Integer releaseCharge(int chargeAmount) {
+        var delta = (chargeAmount > charge) ? chargeAmount : charge;
+        charge -= delta;
+        return delta;
     }
 
     public Integer charge() {
