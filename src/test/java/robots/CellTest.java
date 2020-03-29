c70672812e77fe98040457d43c148b99dd97ddf4
vitek999 <vitek2012rus@gmail.com>
2020-03-29 09:20:59 +0400

    Added double Sided set neighbors cells test, two times set neighbors  in one direction

--- src/test/java/robots/CellTest.java
+++ src/test/java/robots/CellTest.java
@@ -84,8 +84,29 @@ class CellTest {
         assertEquals(cell, neighborCell.neighborCell(direction.getOppositeDirection()));
     }
 
-    // TODO: Левый задаётся правому, а правый левому.
-    // TODO: Установка нового соседа в том же направлении, если уже есть.
+
+    @Test
+    public void test_setNeighborCell_doubleSided() {
+        Cell neighborCell = new Cell();
+        Direction direction = Direction.NORTH;
+
+        cell.setNeighbor(neighborCell, direction);
+        neighborCell.setNeighbor(cell, direction.getOppositeDirection());
+        assertEquals(neighborCell, cell.neighborCell(direction));
+        assertEquals(cell, neighborCell.neighborCell(direction.getOppositeDirection()));
+    }
+
+    @Test
+    public void test_setNeighborCell_twoTimesInOneDirection() {
+        Cell neighborCell = new Cell();
+        Cell anotherCell = new Cell();
+        Direction direction = Direction.NORTH;
+
+        cell.setNeighbor(neighborCell, direction);
+        assertThrows(IllegalArgumentException.class, () -> cell.setNeighbor(anotherCell, direction));
+        assertEquals(neighborCell, cell.neighborCell(direction));
+        assertEquals(cell, neighborCell.neighborCell(direction.getOppositeDirection()));
+    }
 
     @Test
     public void test_isNeighbor_WhenNeighborCellExists() {
