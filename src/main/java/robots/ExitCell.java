3356dd96cabfbb6ee16bc5056f4e24fc40c6bc8e
vitek999 <vitek2012rus@gmail.com>
2020-03-29 11:30:27 +0400

    implement exit cell

--- src/main/java/robots/ExitCell.java
+++ src/main/java/robots/ExitCell.java
@@ -5,13 +5,32 @@ import java.util.List;
 
 public class ExitCell extends Cell {
 
+    private static final long SLEEP_TIME = 1000L;
+
     private List<Robot> teleportedRobots = new ArrayList<>();
 
     public List<Robot> getTeleportedRobots() {
         return new ArrayList<>(teleportedRobots);
     }
 
+    @Override
+    public void setRobot(Robot robot) {
+        super.setRobot(robot);
+        waitTeleportation();
+        teleportRobot();
+    }
+
     private void teleportRobot() {
-        // TODO: implement
+        Robot robot = takeRobot();
+        teleportedRobots.add(robot);
+        // TODO: send event, that robot is teleported;
+    }
+
+    private void waitTeleportation() {
+        try {
+            Thread.sleep(SLEEP_TIME);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
