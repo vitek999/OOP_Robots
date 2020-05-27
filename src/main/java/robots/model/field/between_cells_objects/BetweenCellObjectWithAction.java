c715d9376d1aae68f5005ce3fe72a7a4bc23d1ac
vitek999 <vitek2012rus@gmail.com>
2020-05-27 11:25:04 +0400

    Добавил BetweenCellObjectWithAction

---
+++ src/main/java/robots/model/field/between_cells_objects/BetweenCellObjectWithAction.java
@@ -0,0 +1,8 @@
+package robots.model.field.between_cells_objects;
+
+public abstract class BetweenCellObjectWithAction {
+
+    public abstract void perform();
+
+    public abstract int actionCost();
+}
