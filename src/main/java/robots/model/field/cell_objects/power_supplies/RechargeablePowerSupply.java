2be73fabd505d7f3640048bd05d5cb9432e71919
vitek999 <vitek2012rus@gmail.com>
2020-05-27 21:35:02 +0400

    Добавлен RechargeablePowerSupply

---
+++ src/main/java/robots/model/field/cell_objects/power_supplies/RechargeablePowerSupply.java
@@ -0,0 +1,19 @@
+package robots.model.field.cell_objects.power_supplies;
+
+import org.jetbrains.annotations.NotNull;
+
+public abstract class RechargeablePowerSupply extends PowerSupply{
+    public RechargeablePowerSupply(int charge, int maxCharge) {
+        super(charge, maxCharge);
+    }
+
+    public void charge(@NotNull PowerSupply source) {
+        int amountCharge = maxCharge - charge;
+        int released  = source.releaseCharge(amountCharge);
+        if(released != amountCharge) {
+            released = source.releaseCharge(source.getCharge());
+        }
+        charge += released;
+    }
+
+}
