00889c061b2a5799cc08ae918cdbecd450303b60
vitek999 <vitek2012rus@gmail.com>
2020-05-28 14:54:42 +0400

    Обнрвлена диаграмма и добавлен уникальный идентификатор в ячейку

--- src/main/java/robots/model/field/Cell.java
+++ src/main/java/robots/model/field/Cell.java
@@ -89,5 +89,24 @@ public abstract class Cell implements ImmutableCell {
         betweenCellObject.setPosition(position);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cell cell = (Cell) o;
+        return Objects.equals(uuid, cell.uuid) &&
+                Objects.equals(objectList, cell.objectList) &&
+                Objects.equals(neighborCells, cell.neighborCells) &&
+                Objects.equals(neighborBetweenCellObjects, cell.neighborBetweenCellObjects);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid, objectList, neighborCells.size(), neighborBetweenCellObjects.size());
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
