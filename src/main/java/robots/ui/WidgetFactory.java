f20808d66669e6d9425590341a7e6c44bcfc7aa5
vitek999 <vitek2012rus@gmail.com>
2020-05-29 15:04:51 +0400

    Частично адаптировал WidgetFactory для новой модели

--- src/main/java/robots/ui/WidgetFactory.java
+++ src/main/java/robots/ui/WidgetFactory.java
@@ -2,16 +2,23 @@ package robots.ui;
 
 import org.jetbrains.annotations.NotNull;
 import robots.model.*;
+import robots.model.field.BetweenCellObject;
+import robots.model.field.CellObject;
+import robots.model.field.MobileCellObject;
+import robots.model.field.between_cells_objects.Door;
+import robots.model.field.cell_objects.power_supplies.Accumulator;
+import robots.model.field.cell_objects.power_supplies.PowerSupply;
+import robots.model.field.cell_objects.power_supplies.Windmill;
+import robots.model.field.cells.CellWithPowerSupply;
 import robots.model.field.cells.ExitCell;
 import robots.model.field.cell_objects.power_supplies.Battery;
 import robots.model.field.cell_objects.Robot;
 import robots.model.field.Cell;
 import robots.model.field.between_cells_objects.WallSegment;
+import robots.ui.block.BetweenCellsWidget;
+import robots.ui.block.BlockWidget;
 import robots.ui.block.WallWidget;
-import robots.ui.cell.BatteryWidget;
-import robots.ui.cell.CellWidget;
-import robots.ui.cell.ExitWidget;
-import robots.ui.cell.RobotWidget;
+import robots.ui.cell.*;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -21,77 +28,81 @@ import java.util.Map;
 
 public class WidgetFactory {
 
-//    private final Map<Cell, CellWidget> cells = new HashMap<>();
-//    private final Map<Robot, RobotWidget> robots = new HashMap<>();
-//    private final Map<Battery, BatteryWidget> batteries = new HashMap<>();
-//    private final Map<WallSegment, WallWidget> walls = new HashMap<>();
-//    private final List<Color> usedColors = new ArrayList<>();
-//
-//    public CellWidget create(@NotNull Cell cell) {
-//        if(cells.containsKey(cell)) return cells.get(cell);
-//
-//        CellWidget item = (cell instanceof ExitCell) ? new ExitWidget() : new CellWidget();
-//
-//        Robot robot = (Robot) cell.getMobileCellObject();
-//        if(robot != null) {
-//            RobotWidget robotWidget = create(robot);
-//            item.addItem(robotWidget);
-//        }
-//
-//        Battery battery = cell.getBattery();
-//        if(battery != null) {
-//            BatteryWidget batteryWidget = create(battery);
-//            item.addItem(batteryWidget);
-//        }
-//
-//        cells.put(cell, item);
-//        return item;
-//    }
-//
-//    public CellWidget getWidget(@NotNull Cell cell) {
-//        return cells.get(cell);
-//    }
-//
-//    public RobotWidget create(@NotNull Robot robot) {
-//        if(robots.containsKey(robot)) return robots.get(robot);
-//
-//        Color color = (usedColors.contains(Color.RED)) ? Color.BLUE : Color.RED;
-//        usedColors.add(color);
-//
-//        RobotWidget item = new RobotWidget(robot, color);
-//        robots.put(robot, item);
-//        return item;
-//    }
-//
-//    public RobotWidget getWidget(@NotNull Robot robot) {
-//        return robots.get(robot);
-//    }
-//
-//    public BatteryWidget create(@NotNull Battery battery) {
-//        if(batteries.containsKey(battery)) return batteries.get(battery);
-//
-//        BatteryWidget item = new BatteryWidget(battery);
-//        batteries.put(battery, item);
-//        return item;
-//    }
-//
-//    public BatteryWidget getWidget(@NotNull Battery battery) {
-//        return batteries.get(battery);
-//    }
-//
-//    public void remove(Battery battery) {
-//        batteries.remove(battery);
-//    }
-//
-//    public WallWidget create(@NotNull WallSegment wallSegment, Orientation orientation) {
-//        if(walls.containsKey(wallSegment)) return walls.get(wallSegment);
-//
-//        WallWidget item = new WallWidget(orientation);
-//        walls.put(wallSegment, item);
-//        return item;
-//    }
-//
-//    public WallWidget getWidget(@NotNull WallSegment wallSegment) {
-//        return walls.get(wallSegment);
-//    }
+    private final Map<Cell, CellWidget> cells = new HashMap<>();
+    private final Map<CellObject, CellItemWidget> cellObjects = new HashMap<>();
+    private final Map<BetweenCellObject, BlockWidget> betweenCellObjects = new HashMap<>();
+    private final List<Color> usedColors = new ArrayList<>();
+    public CellWidget create(@NotNull Cell cell) {
+        if(cells.containsKey(cell)) return cells.get(cell);
+
+        CellWidget item = (cell instanceof ExitCell) ? new ExitWidget() : new CellWidget();
+
+        MobileCellObject mobileCellObject = cell.getMobileCellObject();
+        if(mobileCellObject != null) {
+            CellItemWidget robotWidget = create(mobileCellObject);
+            item.addItem(robotWidget);
+        }
+
+        if(cell instanceof CellWithPowerSupply) {
+            PowerSupply powerSupply = ((CellWithPowerSupply) cell).getPowerSupply();
+            if (powerSupply != null) {
+                CellItemWidget powerSupplyWidget = create(powerSupply);
+                item.addItem(powerSupplyWidget);
+            }
+        }
+
+        cells.put(cell, item);
+        return item;
+    }
+
+    public CellWidget getWidget(@NotNull Cell cell) {
+        return cells.get(cell);
+    }
+
+    public CellItemWidget create(@NotNull CellObject cellObject) {
+        if(cellObjects.containsKey(cellObject)) return cellObjects.get(cellObject);
+
+        CellItemWidget createdWidget = null;
+        if(cellObject instanceof Robot) {
+            Color color = (usedColors.contains(Color.RED)) ? Color.BLUE : Color.RED;
+            usedColors.add(color);
+            createdWidget = new RobotWidget((Robot) cellObject, color);
+        } else if(cellObject instanceof Battery) {
+            createdWidget = new BatteryWidget((Battery) cellObject);
+        } else if(cellObject instanceof Accumulator) {
+            // TODO: create accumulator widget
+        } else if(cellObject instanceof Windmill) {
+            // TODO: create accumulator widget
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+        cellObjects.put(cellObject, createdWidget);
+        return createdWidget;
+    }
+
+    public CellItemWidget getWidget(@NotNull CellObject cellObject) {
+        return cellObjects.get(cellObject);
+    }
+
+    public BlockWidget create(@NotNull BetweenCellObject betweenCellObject, Orientation orientation) {
+        if (betweenCellObjects.containsKey(betweenCellObject)) return betweenCellObjects.get(betweenCellObject);
+
+        BlockWidget createdBlockWidget = null;
+
+        if(betweenCellObject instanceof WallSegment) {
+            createdBlockWidget = new WallWidget(orientation);
+        } else if (betweenCellObject instanceof Door) {
+            // TODO create DoorWidget
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+        betweenCellObjects.put(betweenCellObject, createdBlockWidget);
+        return createdBlockWidget;
+    }
+
+    public BlockWidget getWidget(@NotNull BetweenCellObject betweenCellObject) {
+        return betweenCellObjects.get(betweenCellObject);
+    }
 }
